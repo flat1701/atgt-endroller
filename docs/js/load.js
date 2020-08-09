d97de04d38674096adc9b25c2e9e18aae9beec4f
flat1701 <flat1701@gmail.com>
2020-08-09 19:03:11 +0900

    Update load.js

--- docs/js/load.js
+++ docs/js/load.js
@@ -23,6 +23,15 @@ $("#result").append('<div class="monme">'+
 (this.debug.length > 0 ?'<div class="debug">検証とデバッグ：<br>'+this.debug.join("<br>")+'</div><br>' : '')+
 (this.tensai.length > 0 ? '<div class="tensai">甜菜：<br>'+this.tensai.join("<br>")+'</div>' : ''));
 });
+$("#result").append('<div class="monme">'+
+'<div class="title">29匁 ポツンと一人で行くところ</div>'+
+'<div class="locate">現地：<br>福岡県福岡市 福岡空港駅５番出口</div><br>'+
+'<div class="w3w">what3words：<br>///はりあい・そして・おれい</div><br>'+
+'<div class="author">作問・ロケハン：<br>なみ<br>脚本：<br>Ren<br>'+
+'現地ガムテ・ロケハン・デザイン補佐：<br>みのり<br>デザイン：<br>kee<br>'+
+'デザイン補佐：<br>海老<br>fff</div><br>'+
+'<div class="debug">デバッグ：<br>さやちぃ。</div>'+
+'<div class="tensai">甜菜：<br>@rare_shana3<br>@yuna_sashimi<br>@Euex<br>@swi_gin</div></div>');
 $("#result").append('<div class="monme yusha">勇者(1~13,15～28匁):ゆうしゃくん<br>'+
 'ゆうしゃくん総応援回数:1689回<br>'+
 '14匁の勇者:マイクラ班<br>'+
